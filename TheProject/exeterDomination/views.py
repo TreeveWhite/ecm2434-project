1148e234ff2946df43fec868f2ede27d88fc9ea2
Treeve <treevew@gmail.com>
2022-02-16 20:28:54 +0000

    Added Index View

--- TheProject/exeterDomination/views.py
+++ TheProject/exeterDomination/views.py
@@ -1,3 +1,7 @@
+from urllib import request
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request : request) -> HttpResponse:
+    context = {}
+    return render(request, "exeterDomination/homePage.html", context)
