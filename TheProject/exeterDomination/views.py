5dbd1c1a50089a9490161d77b938a5545bf399f8
Treeve <treevew@gmail.com>
2022-02-16 20:56:09 +0000

    Added backend for other pages

--- TheProject/exeterDomination/views.py
+++ TheProject/exeterDomination/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def index(request : request) -> HttpResponse:
     context = {}
     return render(request, "exeterDomination/homePage.html", context)
+
+def about(request : request) -> HttpResponse:
+    context = {}
+    return render(request, "exeterDomination/aboutPage.html", context)
+
+def login(request : request) -> HttpResponse:
+    context = {}
+    return render(request, "exeterDomination/loginPage.html", context)
+
+def signup(request : request) -> HttpResponse:
+    context = {}
+    return render(request, "exeterDomination/signUpPage.html", context)
+
+def game(request : request) -> HttpResponse:
+    context = {}
+    return render(request, "exeterDomination/gamePage.html", context)
