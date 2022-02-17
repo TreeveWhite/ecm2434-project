264c5922210f80ae792a4e895911c586131c3ac7
Treeve <treevew@gmail.com>
2022-02-17 13:58:20 +0000

    started making updates

--- TheProject/exeterDomination/models.py
+++ TheProject/exeterDomination/models.py
@@ -7,13 +7,15 @@ from django.db import models
 
 class Users(models.Model):
     name = models.TextField()
-    username = models.TextField()
-    password = models.TextField()
+    user_username = models.TextField()
+    user_password = models.TextField()
 
 class Locations(models.Model):
     name = models.TextField()
-    longitude = models.IntegerField()
-    latitude = models.IntegerField()
-    claimedBy = models.IntegerField()
+    p1_longitude = models.IntegerField()
+    p1_latitude = models.IntegerField()
+    p2_longitude = models.IntegerField()
+    p2_latitude = models.IntegerField()
+    claimedBy = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True)
 
 
--- TheProject/exeterDomination/views.py
+++ TheProject/exeterDomination/views.py
@@ -1,6 +1,7 @@
 from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
+from exeterDomination.models import Users, Locations
 
 def index(request : request) -> HttpResponse:
     context = {}
@@ -10,9 +11,19 @@ def about(request : request) -> HttpResponse:
     context = {}
     return render(request, "exeterDomination/aboutPage.html", context)
 
-def login(request : request) -> HttpResponse:
-    context = {}
-    return render(request, "exeterDomination/loginPage.html", context)
+def login(request : request, username : str, password : str) -> HttpResponse:
+
+    returnPage  = render(request, "exeterDomination/loginPage.html", {})
+
+    # users = Users.objects.filter()
+
+    # if :
+    #     # Successful Login
+    #     # Set cookie as the unique PK or session ID as unique PK.
+    #     context = {}
+    #     returnPage = render(request, "exeterDomination/gamePage.html", context)
+
+    return returnPage
 
 def signup(request : request) -> HttpResponse:
     context = {}
