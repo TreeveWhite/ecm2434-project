af3ab695bd9a613e8f060b9286f39aadb09acb58
Faris <flk203@exeter.ac.uk>
2022-02-22 06:13:27 +0000

    Added docstrings to all of the test classes / methods.

--- TheProject/exeterDomination/tests/test_integrationWithSelenium.py
+++ TheProject/exeterDomination/tests/test_integrationWithSelenium.py
@@ -23,6 +23,18 @@ class SeleniumLoginTest(StaticLiveServerTestCase, TestCase):
         super().tearDownClass()
 
     def testLoginFunctionality(self):
+        """
+        This function navigates directly to the login page, enters a username and
+        password, and then presses the login button. It then checks that the user
+        is logged in.
+
+        Before Login Implementation:
+            * assert that the page you are taken to after login does not contain
+              Login or Sign Up
+        After Login Implementation:
+            * check for some personal information in the page source, such as
+              username in the top right corner.
+        """
         self.selenium.get(self.live_server_url + "/play/login")
         unameInput = self.selenium.find_element_by_name("uname")
         # Need to tweak this to allow us to use this username in final build
@@ -45,7 +57,7 @@ class SeleniumLoginTest(StaticLiveServerTestCase, TestCase):
 class SignUpWithSeleniumTest(StaticLiveServerTestCase, TestCase):
     """
     This class will test the sign up system with test credentials and check if the
-    user is sent to the correct loacation.
+    user is sent to the correct location.
     """
     @classmethod
     def setUpClass(cls):
@@ -59,6 +71,17 @@ class SignUpWithSeleniumTest(StaticLiveServerTestCase, TestCase):
         super().tearDownClass()
 
     def testSignUpFunctionality(self):
+        """
+        This function loads the signup page, creates a user account, and presses
+        the signup button. It then checks that the user is redirected directly
+        to a page where they are logged in.
+
+        Before Login Implementation:
+            * Checks that Login and Signup are not in the page's source or title
+        After Login Implementation:
+            * Checks that the user is on the correct page, and their username is
+              in the top right corner.
+        """
         self.selenium.get(self.live_server_url + "/play/signup")
         testUsername = self.selenium.find_element_by_name("username")
         testUsername.send_keys("testUser1")
@@ -95,6 +118,10 @@ class testNavigationLinks(StaticLiveServerTestCase, TestCase):
         super().tearDownClass()
 
     def testHomeToAboutBackToHome(self):
+        """
+        This function tests that the navigation buttons from the home
+        page to the about page and back are in working order.
+        """
         self.selenium.get(self.live_server_url + "/play")
         aboutButton = self.selenium.find_element_by_xpath(
             "//input[@class='arcade-font button2']")
@@ -108,6 +135,10 @@ class testNavigationLinks(StaticLiveServerTestCase, TestCase):
         assert self.selenium.current_url == self.live_server_url + "/play/"
 
     def testHomeToPlayBackToHome(self):
+        """
+        This function tests that the navigation buttons from the home
+        page to the play game page and back are in working order.
+        """
         self.selenium.get(self.live_server_url + "/play")
         playButton = self.selenium.find_element_by_xpath(
             "//input[@class='arcade-font button1']")
@@ -122,6 +153,10 @@ class testNavigationLinks(StaticLiveServerTestCase, TestCase):
         assert self.selenium.current_url == self.live_server_url + "/play/"
 
     def testHomeToLeaderboardToHome(self):
+        """
+        This function tests that the navigation buttons from the home
+        page to the leaderboard page and back are in working order.
+        """
         self.selenium.get(self.live_server_url + "/play")
         leaderboardButton = self.selenium.find_element_by_xpath(
             "//input[@class='arcade-font button3']")
@@ -135,6 +170,10 @@ class testNavigationLinks(StaticLiveServerTestCase, TestCase):
         assert self.selenium.current_url == self.live_server_url + "/play/"
 
     def testLeaderboardToLogin(self):
+        """
+        This function tests a likely path for a returning user, who may
+        check the leaderboard and then move to the login page.
+        """
         self.selenium.get(self.live_server_url + "/play/leaderboard")
         loginButton = self.selenium.find_element_by_xpath(
             "//a[contains(@href, 'login')]")
@@ -143,6 +182,11 @@ class testNavigationLinks(StaticLiveServerTestCase, TestCase):
         assert self.selenium.current_url == self.live_server_url + "/play/login"
 
     def testLeaderboardToSignUp(self):
+        """
+        This function tests the most likely path for a new user, who may
+        have originally been curious about who was topping the leaderboard,
+        and has subsequently navigated to the sign up page.
+        """
         self.selenium.get(self.live_server_url + "/play/leaderboard")
         signUpButton = self.selenium.find_element_by_xpath(
             "//a[contains(@href, 'signup')]")
--- TheProject/exeterDomination/tests/test_views.py
+++ TheProject/exeterDomination/tests/test_views.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.test import Client
 
 
 class TestGame(TestCase):
+    """
+    This class tests the 'game' view, where the user can play a wordle
+    like game to capture a point. Anything to do with the login
+    code is merely a placeholder until the login implementation is
+    complete.
+    """
     def setUp(self):
         # Must be changed when sessions are implemented
         User.objects.create(username="test", password="password")
 
     def testGameWithNoLogin(self):
+        """
+        Tests if the user is taken to the login page if they try to
+        visit the gameplay page without being logged in.
+        """
         resp = self.client.get(reverse('game'), follow=True)
         # Change this if redirect location changes later down the line
         self.assertRedirects(resp, '/play/login')
@@ -17,24 +28,43 @@ class TestGame(TestCase):
         # later down the line
 
     def testGameWithLogin(self):
+        """
+        Tests if the user is taken to the gameplay page if they
+        access it while being logged in already.
+        """
         # Must be changed when sessions are implemented
         self.client.login(username="test", password="password")
+        #c = Client(enforce_csrf_checks=True)
+        #c.force_login("test")
         resp = self.client.get(reverse('game'))
         self.assertRedirects(resp, '/play/game')
         self.assertTemplateUsed(resp, "exeterDomination/gamePage.html")
 
 
 class TestLoginPage(TestCase):
+    """
+    This class tests the redirects on the login page.
+    """
     def setUp(self):
         # Must be changed when sessions are implemented
         User.objects.create(username="test", password="password")
 
     def testLoginPageWhileLoggedOut(self):
+        """
+        This function makes sure the user is on the correct
+        page (login page) if they navigate to it and are not
+        logged in.
+        """
         resp = self.client.get(reverse('login'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'exeterDomination/loginPage.html')
 
     def testLoginPageWhileLoggedIn(self):
+        """
+        This function tests to see if the user is redirected
+        to the correct page if they navigate to the login
+        page and are already logged in.
+        """
         # Must be changed when sessions are implemented
         self.client.login(username="test", password="password")
         resp = self.client.get(reverse('login'))
@@ -44,11 +74,19 @@ class TestLoginPage(TestCase):
 
 
 class TestSignUpPage(TestCase):
+    """
+    This class tests the redirects on the signUp page.
+    """
     def setUp(self):
         # Must be changed when sessions are implemented
         User.objects.create(username="test", password="password")
 
     def testSignUpPageWhileSignedIn(self):
+        """
+        This function makes sure a user is redirected to
+        the appropriate page if they navigate to the
+        signup page while already logged in.
+        """
         # Must be changed when sessions are implemented
         self.client.login(username="test", password="password")
         resp = self.client.get(reverse('signup'))
@@ -57,6 +95,11 @@ class TestSignUpPage(TestCase):
         self.assertTemplateUsed(resp, 'exeterDomination/gamePage.html')
 
     def testSignUpPageWhileSignedOut(self):
+        """
+        This function just tests if a user remains on the
+        correct page (signup page) when they navigate to it
+        while signed out.
+        """
         resp = self.client.get(reverse('signup'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'exeterDomination/signUpPage.html')
