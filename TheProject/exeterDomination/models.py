97a112faac5f16cce87fa6603127d68424f4ec39
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-02-22 12:02:56 +0000

    Chnaged Database

--- TheProject/exeterDomination/models.py
+++ TheProject/exeterDomination/models.py
@@ -6,16 +6,16 @@
 from django.db import models
 
 class Users(models.Model):
-    name = models.TextField()
     username = models.TextField()
     password = models.TextField()
+    lastPos = models.ForeignKey('CoOrds', on_delete=models.SET_NULL, null=True)
 
 class Locations(models.Model):
     name = models.TextField()
-    p1Longitude = models.IntegerField()
-    p1Latitude = models.IntegerField()
-    p2Longitude = models.IntegerField()
-    p2Latitude = models.IntegerField()
+    trCoOrd = models.ForeignKey('CoOrds', on_delete=models.SET_NULL, null=True)
+    blCoOrd = models.ForeignKey('CoOrds', on_delete=models.SET_NULL, null=True) 
     claimedBy = models.ForeignKey("Users", on_delete=models.SET_NULL, null=True)
 
-
+class CoOrds(models.Model):
+    longitude = models.IntegerField()
+    latitude = models.IntegerField()
