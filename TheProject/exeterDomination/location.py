dae8b165ee10655363957b810af0b463b6911284
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-02-22 12:43:14 +0000

    Created Location Function

--- TheProject/exeterDomination/location.py
+++ TheProject/exeterDomination/location.py
@@ -1,16 +1,16 @@
 
 
-def posInSquare(top : int, left : int, bottom : int, right : int, posLat : int, posLong : int) -> bool:
-    # inArea = False
-    # # Check if the position longitude is beteen the leftmost and rightmost.
-    # if ((left <= posLong) and (right >= posLong)) or ((left < 0 and right >= 0) and (left <= )):
-    #     # Check if the position latitude is between the top and bottom.
-    #     if (top >= posLat) and (bottom <= posLat):
-    #         inArea = True
+
+def posInRec(top : int, west : int, bottom : int, east : int, posLat : int, posLong : int) -> bool:
+    """
+    This function is used to check if a position is within a rectangle defined by its top west and bottom
+    east corners.
+
+    All positions use longitude and latitude coordinates.
+    """
+    inArea = False
+
+    if ((posLat <= top and posLat >= bottom) and (posLong <= west and posLong >= east)):
+        inArea = True
     
-    # return inArea
-
-    pass
-
-
-# This works for when not points around 0 swapover.
+    return inArea
