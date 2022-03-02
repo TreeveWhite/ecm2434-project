7da6749e961212ae2fda59b569d13740d3ff3a47
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-03-02 21:36:33 +0000

    Removed print statements

--- TheProject/exeterDomination/location.py
+++ TheProject/exeterDomination/location.py
@@ -12,6 +12,16 @@ def posInRec(userID: int, posLat: int, posLong: int) -> bool:
     claimed = False
 
     for location in Locations.objects.all():
+        # print(location.name)
+        # print("Player Lat:", posLat)
+        # print("Player Long:", posLong)
+        # print("Location Lat Top:", location.trCoOrd.latitude, " Location Lat Bottom: ", location.blCoOrd.latitude)
+        # print(posLat <= location.trCoOrd.latitude and posLat >= location.blCoOrd.latitude)
+
+        # print("Location Long left:", location.blCoOrd.longitude, " Location Long Right: ", location.trCoOrd.longitude)
+        # print(posLong <= location.trCoOrd.longitude and posLong >= location.blCoOrd.longitude)
+        # print()
+        
         if ((posLat <= location.trCoOrd.latitude and posLat >= location.blCoOrd.latitude) and (posLong <= location.trCoOrd.longitude and posLong >= location.blCoOrd.longitude)):
             location.claimedBy = User.objects.get(pk=userID)
             location.save()
