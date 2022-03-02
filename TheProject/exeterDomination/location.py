d4667398e5546d0e8e35207303e53104587040de
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-03-02 15:28:16 +0000

    Fixed Function

--- TheProject/exeterDomination/location.py
+++ TheProject/exeterDomination/location.py
@@ -10,15 +10,26 @@ def posInRec(userID : int, posLat: int, posLong: int) -> bool:
     All positions use longitude and latitude coordinates.
     """
     claimed = False
+
+    # print("Player Lat: ", posLat)
+    # print("Player Long: ", posLong)
+
     for location in Locations.objects.all():
 
-        print(location.trCoOrd.latitude)
+        # print("Lat left: ", location.blCoOrd.latitude, "|  Lat right: ", location.trCoOrd.latitude)
+        # print("Long Bottom: ", location.blCoOrd.longitude, "|  Long Top: ", location.trCoOrd.longitude)
 
-        if ((posLat <= location.trCoOrd.latitude and posLat >= location.blCoOrd.latitude) and (posLong <= location.blCoOrd.longitude and posLong >= location.blCoOrd.longitude)):
+        # print("Lat check")
+        # print(posLat <= location.trCoOrd.latitude and posLat >= location.blCoOrd.latitude)
+
+        # print("Long check")
+        # print(posLong <= location.trCoOrd.longitude and posLong >= location.blCoOrd.longitude)
+
+        if ((posLat <= location.trCoOrd.latitude and posLat >= location.blCoOrd.latitude) and (posLong <= location.trCoOrd.longitude and posLong >= location.blCoOrd.longitude)):
             claimed = True
-            
             break
 
     location.claimedBy = User.objects.get(pk=userID)
+    location.save()
 
     return claimed
