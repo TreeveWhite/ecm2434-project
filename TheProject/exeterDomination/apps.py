3fbf45e6619a60ffea756d3dbaba61aad41ea9b7
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-03-02 23:18:39 +0000

    Added docstrings

--- TheProject/exeterDomination/apps.py
+++ TheProject/exeterDomination/apps.py
@@ -1,6 +1,16 @@
+"""
+alarms.py
+=======================================
+This module contains the ExeterdominationConfit class that enables django to see the 
+exeterDomination app as an app in the system.
+"""
 from django.apps import AppConfig
 
-
 class ExeterdominationConfig(AppConfig):
+    """
+    This class is the confirguration class for the exeterDomination app and 
+    specifies the defult_auto_field attribute and the name attribute.
+    """
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exeterDomination'
