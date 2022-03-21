4536a3ac00807070ddf17b9cdc2e08fa4ae32b76
Faris L Kapes <70965394+flkapes@users.noreply.github.com>
2022-03-21 14:32:45 +0000

    Update manage.py

--- TheProject/manage.py
+++ TheProject/manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    print(sys.path)
     os.environ['DJANGO_SETTINGS_MODULE'] = 'TheProject.settings'
     try:
         from django.core.management import execute_from_command_line
