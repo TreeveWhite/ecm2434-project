5fc638c4f7b44af74e7f012c669e7a79a809c9fd
Faris <flk203@exeter.ac.uk>
2022-03-23 02:35:09 +0000

    attempt1-2

--- TheProject/TheProject/settings.py
+++ TheProject/TheProject/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
+#STATICFILES_DIRS = (Path(BASE_DIR).joinpath('static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
