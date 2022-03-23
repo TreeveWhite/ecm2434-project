903fb012a9e5da1515d2f4e2ac974c2391e020d2
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-03-23 00:26:42 +0000

    removed extra static

--- TheProject/TheProject/static/randomGame.js
+++
@@ -1,6 +0,0 @@
-var jsFiles = ['/static/compSciWordle.js', '/static/rockPaperScissors.js']
-//var jsFiles = ['/static/rockPaperScissors.js'];
-var index = Math.floor(Math.random()*jsFiles.length);
-var script = document.createElement('script');
-script.src = jsFiles[index];
-document.getElementById('gameContainer').appendChild(script)
