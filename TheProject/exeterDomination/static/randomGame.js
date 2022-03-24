b10a88c95e3aeec871494bf3c1b421f8ff5e9d9e
Ethan Gallagher <ethan@Ethans-MacBook-Pro.local>
2022-03-24 11:14:21 +0000

    Comments

--- TheProject/exeterDomination/static/randomGame.js
+++ TheProject/exeterDomination/static/randomGame.js
@@ -1,5 +1,8 @@
+//The array of the game files
 var jsFiles = ['/static/compSciWordle.js', '/static/rockPaperScissors.js', '/static/tictactoe.js']
 var index = Math.floor(Math.random()*jsFiles.length);
 var script = document.createElement('script');
+//Creates new script with the random source
 script.src = jsFiles[index];
+//Appends the HTML container with the random script source
 document.getElementById('gameContainer').appendChild(script)
